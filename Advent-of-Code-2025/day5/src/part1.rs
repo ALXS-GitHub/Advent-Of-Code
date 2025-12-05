dcbbb90c9a01ef3d9c21ef16540383773a684747
ALXS-GitHub <alxs.mnch@gmail.com>
2025-12-05 06:17:40 +0100

    day5

--- Advent-of-Code-2025/day5/src/part1.rs
+++ Advent-of-Code-2025/day5/src/part1.rs
@@ -1,3 +1,46 @@
+pub struct Range {
+    pub start: i64,
+    pub end: i64,
+}
+
+pub fn parse_input(input: &Vec<String>) -> (Vec<Range>, Vec<i64>) {
+    let mut ranges: Vec<Range> = Vec::new();
+    let mut ranges_part = true;
+    let mut ingredients: Vec<i64> = Vec::new();
+
+    for line in input {
+        if line.is_empty() {
+            ranges_part = false;
+            continue;
+        }
+        if ranges_part {
+            let parts: Vec<&str> = line.split('-').collect();
+            let start: i64 = parts[0].parse().unwrap();
+            let end: i64 = parts[1].parse().unwrap();
+            ranges.push(Range { start, end });
+        } else {
+            let ingredient: i64 = line.parse().unwrap();
+            ingredients.push(ingredient);
+        }
+    }
+
+    return (ranges, ingredients);
+}
+
 pub fn part1(input: &Vec<String>) -> i64 {
-    return 0;
+    let (ranges, ingredients) = parse_input(input);
+    let mut fresh_count = 0;
+    for ingredient in ingredients {
+        let mut is_fresh = false;
+        for range in &ranges {
+            if ingredient >= range.start && ingredient <= range.end {
+                is_fresh = true;
+                break;
+            }
+        }
+        if is_fresh {
+            fresh_count += 1;
+        }
+    }
+    return fresh_count;
 }
--- Advent-of-Code-2025/day5/src/part2.rs
+++ Advent-of-Code-2025/day5/src/part2.rs
@@ -1,3 +1,65 @@
+#[derive(Clone, Debug)]
+pub struct Range {
+    pub start: i64,
+    pub end: i64,
+}
+
+pub fn parse_input(input: &Vec<String>) -> (Vec<Range>, Vec<i64>) {
+    let mut ranges: Vec<Range> = Vec::new();
+    let mut ranges_part = true;
+    let mut ingredients: Vec<i64> = Vec::new();
+
+    for line in input {
+        if line.is_empty() {
+            ranges_part = false;
+            continue;
+        }
+        if ranges_part {
+            let parts: Vec<&str> = line.split('-').collect();
+            let start: i64 = parts[0].parse().unwrap();
+            let end: i64 = parts[1].parse().unwrap();
+            ranges.push(Range { start, end });
+        } else {
+            let ingredient: i64 = line.parse().unwrap();
+            ingredients.push(ingredient);
+        }
+    }
+
+    return (ranges, ingredients);
+}
+
+pub fn merge_ranges(ranges: &Vec<Range>) -> Vec<Range> {
+    if ranges.is_empty() {
+        return Vec::new();
+    }
+
+    let mut sorted_ranges = ranges.clone();
+    sorted_ranges.sort_by_key(|r| r.start);
+
+    let mut merged: Vec<Range> = Vec::new();
+    let mut current_range = sorted_ranges[0].clone();
+
+    for range in sorted_ranges.iter().skip(1) {
+        if range.start <= current_range.end {
+            current_range.end = current_range.end.max(range.end);
+        } else {
+            merged.push(current_range);
+            current_range = range.clone();
+        }
+    }
+    merged.push(current_range);
+
+    return merged;
+}
+
 pub fn part2(input: &Vec<String>) -> i64 {
-    return 0;
+    let (ranges, _) = parse_input(input);
+    let merged_ranges = merge_ranges(&ranges);
+
+    let mut total_ranges_length = 0;
+    for range in &merged_ranges {
+        total_ranges_length += range.end - range.start + 1;
+    }
+
+    return total_ranges_length;
 }
